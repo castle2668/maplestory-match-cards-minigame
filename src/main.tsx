7210f46f3d21ff1be4147da52577b50694ac88b6
Sean Huang <castle2668@gmail.com>
2024-09-26 23:39:38 +0800

    Add react router v6 and dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { ThemeProvider } from "./components/theme-provider";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/settings",
+    element: <Settings />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Home;
