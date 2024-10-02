33f06fee925f4ad99899145c47eb9cafa1f47f15
Sean Huang <castle2668@gmail.com>
2024-10-02 18:29:31 +0800

    Update error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,20 +1,39 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
+import BulletinBoardImage from "../assets/images/bulletin-board.png";
+
 const ErrorPage: React.FC = () => {
   const error = useRouteError();
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div>
-        <h1>Oops!</h1>
-        <h2>{error.status}</h2>
-        <p>{error.statusText}</p>
-        {error.data?.message && <p>{error.data.message}</p>}
+  const ErrorMessage = () => {
+    if (isRouteErrorResponse(error)) {
+      return (
+        <div>
+          <h1>Oops!</h1>
+          <h2>{error.status}</h2>
+          <p>{error.statusText}</p>
+          {error.data?.message && <p>{error.data.message}</p>}
+        </div>
+      );
+    } else {
+      return <div>Oops</div>;
+    }
+  };
+
+  return (
+    <div className="relative w-full h-[calc(100vh-212px)]">
+      <div className="absolute -bottom-1 left-0">
+        <div className="text-white font-bold absolute top-[150px] left-[175px]">
+          <ErrorMessage />
+        </div>
+        <img
+          src={BulletinBoardImage}
+          alt="Bulletin Board Image"
+          className="w-[565px] h-[516px]"
+        />
       </div>
-    );
-  } else {
-    return <div>Oops</div>;
-  }
+    </div>
+  );
 };
 
 export default ErrorPage;
